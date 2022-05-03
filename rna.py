77801f1ef228c8f50a82d64ca5644ee1f8268a8f
Patrick Plettner <17936703+pplettner@users.noreply.github.com>
2022-05-02 17:46:26 -0700

    Add RNA solution

---
+++ rna.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+import argparse
+
+def dna_to_rna(string):
+    trans_table = str.maketrans('ACGT','ACGU')
+    rna_string = string.upper().translate(trans_table)
+    return rna_string
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset', type=argparse.FileType('r'))
+args = parser.parse_args()
+
+dna_string = args.dataset.readline().strip()
+rna_string = dna_to_rna(dna_string)
+
+print (rna_string)
