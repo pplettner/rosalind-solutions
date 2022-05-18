7c89dac6aeeffb3587d5bfd6fb2305ead9490460
Patrick Plettner <17936703+pplettner@users.noreply.github.com>
2022-05-18 11:38:49 -0700

    Minor update SIGN solution

--- sign.py
+++ sign.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 import argparse
-from itertools import combinations,permutations
+from itertools import chain,combinations,permutations
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dataset', type=argparse.FileType('r'))
@@ -9,20 +9,25 @@ args = parser.parse_args()
 
 n = int(args.dataset.readline().strip())
 
-perms = list(permutations(range(1,n+1), n))
+perms = list(
+    permutations(range(1,n+1), n)
+)
 
-index_combos = []
-for i in range(1,n+1):
-    index_combos.extend(combinations(range(n), i))
+index_combos = list(
+    chain.from_iterable(
+        combinations(range(n), i) for i in range(1,n+1)
+    )
+)
 
 print ( len(perms) * (len(index_combos) + 1) )
 
 for p in perms:
+
     print (' '.join([str(x) for x in p]))
 
     for ic in index_combos:
         signed_perm = list(p)
         for i in ic:
             signed_perm[i] *= -1
-
+        
         print (' '.join([str(x) for x in signed_perm]))
