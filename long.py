e97f3f7bbb85b3d0d53da09367fc8f79113bce72
Patrick Plettner <17936703+pplettner@users.noreply.github.com>
2022-05-30 11:06:54 -0700

    LONG solution refactoring

--- long.py
+++ long.py
@@ -4,45 +4,43 @@ import argparse
 from itertools import permutations
 from utils import read_fasta
 
-def overlap_position(s1,s2):
+def overlap_position(seq1, seq2):
     '''
     Determines whether the end of s1 overlaps with the start of s2.
     It requires that the overlap length be >1/2 of the total string length.
     '''
-    mid = int( len(s2)/2 ) + 1
-    start = s1.find(s2[:mid])
+    mid = int( len(seq2)/2 ) + 1
+    start = seq1.find(seq2[:mid])
 
     if start != -1:
-        chars_left = len(s1) - mid - start
-        if s1[-chars_left:] == s2[mid:mid+chars_left]:
+        chars_left = len(seq1) - mid - start
+        if seq1[-chars_left:] == seq2[mid:mid+chars_left]:
             return mid+chars_left
 
     return 0
 
-    
 parser = argparse.ArgumentParser()
 parser.add_argument('dataset', type=argparse.FileType('r'))
 args = parser.parse_args()
 
 input_seqs = [seq for (_,seq) in read_fasta(args.dataset)]
-next_seq = dict()
-overlap_pos = dict()
+best_overlap = dict()
 
-for (a,b) in permutations(input_seqs,2):
-    if a not in next_seq:
-        i = overlap_position(a,b)
-        if i > 0:
-            next_seq[a] = b
-            overlap_pos[a] = i
+for (seq1, seq2) in permutations(input_seqs,2):
+    if seq1 not in best_overlap:
+        pos = overlap_position(seq1, seq2)
+        if pos > 0:
+            best_overlap[seq1] = (seq2, pos)
 
 
-(first_seq,) = set(input_seqs) - set(next_seq.values())
-consensus = curr = first_seq
+(first_seq,) = set(input_seqs) - set([x[0] for x in best_overlap.values()])
+consensus = seq = first_seq
 
-while (successor := next_seq.get(curr)) is not None:    
-    i = overlap_pos[curr]
-    consensus += successor[i:]
-
-    curr = successor
+while (overlap := best_overlap.get(seq)) is not None:    
+    successor = overlap[0]
+    pos = overlap[1]
+    consensus += successor[pos:]
+    seq = successor
 
 print (consensus)
+
